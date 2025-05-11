d8725b3d1a72813d5a9ce59a39325df150d69099
Quinten ‎Delaere <quinten.delaere@lln.athena-heule.be>
2025-05-11 20:03:59 +0200

    PushNotifications: Updated service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.worker.js')
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js')
 
 workbox.routing.registerRoute(
